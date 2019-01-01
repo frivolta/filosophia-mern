5ade7bcc5c8af5d65474892b683c940332d07424
frivolta <rivoltafilippo@gmail.com>
2019-01-01 05:26:05 +0100

    Ready for prod testing register

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OK_KEY
+  mongoURI: 'mongodb://root:qX0U4vWv@ds253783.mlab.com:53783/mern-start-boilerplate',
+  secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -33,6 +33,15 @@ app.use ('/api/users', users);
 app.use ('/api/quotes', quotes);
 
 
+/** Serve static assets if in production*/
+if(process.env.NODE_ENV === 'production'){
+    //Set Static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 /** Start and Listen */
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log (`Server running on port ${port}`));
