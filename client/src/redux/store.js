1a9ed9227e840a09f9f8a03944c8748f1d3c9297
frivolta <rivoltafilippo@gmail.com>
2019-01-02 00:14:44 +0100

    Fix redux devtools ext bug

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -9,13 +9,20 @@ import rootReducer from './reducers';
 const initialState = {}
 const middleware = [thunk];
 
+// In development, use the browser's Redux dev tools extension if installed
+const enhancers = [];
+const isDevelopment = process.env.NODE_ENV === 'development';
+if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
+  enhancers.push(window.devToolsExtension());
+}
+ 
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+    ...enhancers,
+  ),
+);
 
 export default store;
